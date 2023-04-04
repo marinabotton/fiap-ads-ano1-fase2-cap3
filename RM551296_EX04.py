813acdbac79fe6be2df5cfab512b7e2522257865
Marina Botton <90083919+marinabotton@users.noreply.github.com>
2023-04-04 14:31:59 -0400

    Update RM551296_EX04.py

--- RM551296_EX04.py
+++ RM551296_EX04.py
@@ -2,15 +2,15 @@
 fatorial = 1
 
 # Recebe os minutos atuais do usuário
-minutos = int(input("Digite os minutos atuais da máquina: "))
+minutos = int(input("> > > Digite os minutos atuais da máquina: "))
 
 # Validar dados de entrada
 while minutos < 0 or minutos > 59:
     minutos = int(input("> > > Número inválido! Informe os minutos atuais da máquina: "))
 
 # Calcula o fatorial dos minutos
-for x in range(1, minutos + 1):
+for x in range(2, minutos + 1):
     fatorial *= x
 
 # Exibe a senha de desbloqueio
-print("A senha de desbloqueio é: LIBERDADE{}".format(fatorial))
+print("> > > A senha de desbloqueio é: LIBERDADE{} < < <".format(fatorial))
