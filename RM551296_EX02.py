9a2a948c6e3acad9cf281972c1519074ef0ada7f
Marina Botton <90083919+marinabotton@users.noreply.github.com>
2023-04-04 14:25:54 -0400

    Update RM551296_EX02.py

--- RM551296_EX02.py
+++ RM551296_EX02.py
@@ -1,18 +1,28 @@
-# solicita a quantidade de votos de cada dia da semana
-segunda = int(input("Digite a quantidade de votos para segunda-feira: "))
-terca = int(input("Digite a quantidade de votos para terça-feira: "))
-quarta = int(input("Digite a quantidade de votos para quarta-feira: "))
-quinta = int(input("Digite a quantidade de votos para quinta-feira: "))
-sexta = int(input("Digite a quantidade de votos para sexta-feira: "))
+# Solicita a quantidade de votos de cada dia da semana
+segunda = int(input("> > > Digite a quantidade de votos para Segunda-Feira: "))
+terca = int(input("> > > Digite a quantidade de votos para Terça-Feira: "))
+quarta = int(input("> > > Digite a quantidade de votos para Quarta-Feira: "))
+quinta = int(input("> > > Digite a quantidade de votos para Quinta-Feira: "))
+sexta = int(input("> > > Digite a quantidade de votos para Sexta-Feira: "))
 
-# compara os votos e exibe o dia mais votado
+# Compara os votos e exibe o dia mais votado
 if segunda > terca and segunda > quarta and segunda > quinta and segunda > sexta:
-    print("O dia escolhido foi segunda-feira.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("- - - - - - O dia escolhido foi SEGUNDA-FEIRA!  - - - - -")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 elif terca > segunda and terca > quarta and terca > quinta and terca > sexta:
-    print("O dia escolhido foi terça-feira.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("- - - - - - O dia escolhido foi TERÇA-FEIRA!  - - - - - -")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 elif quarta > segunda and quarta > terca and quarta > quinta and quarta > sexta:
-    print("O dia escolhido foi quarta-feira.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("- - - - - - O dia escolhido foi QUARTA-FEIRA! - - - - - -")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 elif quinta > segunda and quinta > terca and quinta > quarta and quinta > sexta:
-    print("O dia escolhido foi quinta-feira.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("- - - - - - O dia escolhido foi QUINTA-FEIRA! - - - - - -")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
 else:
-    print("O dia escolhido foi sexta-feira.")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("- - - - - - O dia escolhido foi SEXTA-FEIRA!  - - - - - -")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
