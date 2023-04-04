dc5cfb94f4ca6da3166adad9086ef2e34046d3c0
Marina Botton <90083919+marinabotton@users.noreply.github.com>
2023-04-04 11:09:18 -0400

    Update RM551296_EX01.py

--- RM551296_EX01.py
+++ RM551296_EX01.py
@@ -1,19 +1,23 @@
-assinatura = input("Digite o tipo de assinatura (Basic, Silver, Gold ou Platinum): ")
+# Entrada de dados para saber qual o nível da assinatura do cliente
+assinatura = input("Digite o nível da assinatura: [1]Basic, [2]Silver, [3]Gold ou [4]Platinum. ")
 
-faturamento_anual = float(input("Digite o faturamento anual do cliente: "))
-
-if assinatura == "Basic":
-    porcentagem = 0.3
-elif assinatura == "Silver":
-    porcentagem = 0.2
-elif assinatura == "Gold":
-    porcentagem = 0.1
-elif assinatura == "Platinum":
-    porcentagem = 0.05
+# Verifica a validade da assinatura
+# Se inválida, apresenta mensagem de erro
+if (assinatura != "1") and (assinatura != "2") and (assinatura != "3") and (assinatura != "4"):
+    print("Assinatura inválida")
+# Se válida, pede a entrada de dados do faturamento anual do cliente
 else:
-    print("Assinatura inválida.")
-    exit()
+    faturamento = float(input("Digite o faturamento anual do cliente: "))
 
-bonus = faturamento_anual * porcentagem
+    # Calcula o bônus com base na assinatura
+    if assinatura == "1":
+        bonus = faturamento * 0.3
+    elif assinatura == "2":
+        bonus = faturamento * 0.2
+    elif assinatura == "3":
+        bonus = faturamento * 0.1
+    elif assinatura == "4":
+        bonus = faturamento * 0.05
 
-print("O valor do bônus que o cliente deve pagar é: R$ {:.2f}".format(bonus))
+    # Mostra o valor do bônus com duas casas decimais
+    print("O valor do bônus que o cliente deve pagar é: R$ {:.2f}".format(bonus))
