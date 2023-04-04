ee686971c31b73880567ccade7c7db8b866fda08
Marina Botton <90083919+marinabotton@users.noreply.github.com>
2023-04-04 14:25:13 -0400

    Update RM551296_EX01.py

--- RM551296_EX01.py
+++ RM551296_EX01.py
@@ -1,13 +1,17 @@
+# Variável criada
+bonus = ""
+
 # Entrada de dados para saber qual o nível da assinatura do cliente
-assinatura = input("Digite o nível da assinatura: [1]Basic, [2]Silver, [3]Gold ou [4]Platinum. ")
+assinatura = input("> > > Digite o nível da assinatura: [1]Basic, [2]Silver, [3]Gold ou [4]Platinum ")
 
 # Verifica a validade da assinatura
 # Se inválida, apresenta mensagem de erro
 if (assinatura != "1") and (assinatura != "2") and (assinatura != "3") and (assinatura != "4"):
-    print("Assinatura inválida")
+    print("> > > Assinatura Inválida < < <")
+
 # Se válida, pede a entrada de dados do faturamento anual do cliente
 else:
-    faturamento = float(input("Digite o faturamento anual do cliente: "))
+    faturamento = float(input("> > > Digite o faturamento anual do cliente: "))
 
     # Calcula o bônus com base na assinatura
     if assinatura == "1":
@@ -20,4 +24,4 @@ else:
         bonus = faturamento * 0.05
 
     # Mostra o valor do bônus com duas casas decimais
-    print("O valor do bônus que o cliente deve pagar é: R$ {:.2f}".format(bonus))
+    print("\n> > > O valor do bônus que o cliente deve pagar é: R$ {:.2f} < < <".format(bonus))
